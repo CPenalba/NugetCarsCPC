f221c9ed9f2bd0581088ac008e54ce9554e34823
CPenalba <carolinapenalbacorpa@gmail.com>
2025-03-19 09:26:12 +0100

    Version 2

--- NugetCarsCPC/Repositories/RepositoryCoches.cs
+++ NugetCarsCPC/Repositories/RepositoryCoches.cs
@@ -12,26 +12,31 @@ namespace NugetCarsCPC.Repositories
     public class RepositoryCoches
     {
         private XDocument document;
-
         public RepositoryCoches()
         {
-            //PARA RECUPERAR UN RECURSO INCRUSTADO NECESITAMOS EL NAMESPACE Y, SI LO TUVIERA
-            //LA CARPETA DONDE ESTUVIERA EL RECURSO INCRSUTADO
+            //PARA RECUPERAR UN RECURSO INCRUSTADO 
+            //NECESITAMOS EL namespace Y, SI LO TUVIERA
+            //LA CARPETA DONDE ESTUVIERA EL RECURSO INCRUSTADO.
             string resourceName = "NugetCarsCPC.coches.xml";
-            //LOS FICHEROS SE RECUPERAN MEDIANTE BYTES, ES DECIR MEDIANTE STREAM
-            Stream stream = this.GetType().Assembly.GetManifestResourceStream(resourceName);
+            //LOS FICHEROS SE RECUPERAN MEDIANTE BYTES, ES DECIR
+            //MEDIANTE Stream
+            Stream stream =
+                this.GetType().Assembly
+                .GetManifestResourceStream(resourceName);
             //COMO ES UN XML FISICO, SE UTILIZA EL METODO LOAD
             this.document = XDocument.Load(stream);
         }
 
         public List<Coche> GetCoches()
         {
-            var consulta = from datos in this.document.Descendants("coche") select datos;
+            var consulta =
+                from datos in this.document.Descendants("coche")
+                select datos;
             List<Coche> cars = new List<Coche>();
             foreach (var tag in consulta)
             {
                 Coche car = new Coche();
-                car.IdCoche = int.Parse(tag.Element("idCoche").Value);
+                car.IdCoche = int.Parse(tag.Element("idcoche").Value);
                 car.Marca = tag.Element("marca").Value;
                 car.Modelo = tag.Element("modelo").Value;
                 car.Imagen = tag.Element("imagen").Value;
@@ -39,5 +44,6 @@ namespace NugetCarsCPC.Repositories
             }
             return cars;
         }
+
     }
 }
